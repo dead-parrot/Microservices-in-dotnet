86837fc75789b398b2864b7a48a6011560b599d4
Rogerio Marinho <rogeriofilho2@gmail.com>
2023-04-23 18:15:46 -0300

    Adicionar arquivos de projeto.

---
+++ HelloMicroservices/Controllers/CurrentDateTimeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMicroservices.Controllers
+{
+    public class CurrentDateTimeController : ControllerBase
+    {
+        [HttpGet("/")]
+        public ActionResult<String> GetCurrenteTimeStamp()
+        {
+            return Ok(DateTime.UtcNow);
+        }
+    }
+}
